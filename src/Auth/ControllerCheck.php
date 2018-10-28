ebea8511bc9497e245749375c5355c602ae843ba
DimNS <atomcms@ya.ru>
2018-10-28 14:28:09 +0500

    refactor: Убран лишний код

--- src/Auth/ControllerCheck.php
+++ src/Auth/ControllerCheck.php
@@ -2,7 +2,7 @@
 /**
  * Проверка валидности сессии
  *
- * @version 26.05.2018
+ * @version 28.10.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 
@@ -10,7 +10,6 @@ namespace Lemurro\Api\Core\Auth;
 
 use Lemurro\Api\Core\Abstracts\Controller;
 use Lemurro\Api\Core\Session;
-use Lemurro\Api\Core\Users\ActionGet as GetUser;
 
 /**
  * Class ControllerCheck
@@ -22,7 +21,7 @@ class ControllerCheck extends Controller
     /**
      * Стартовый метод
      *
-     * @version 26.05.2018
+     * @version 28.10.2018
      * @author  Дмитрий Щербаков <atomcms@ya.ru>
      */
     public function start()
@@ -31,17 +30,9 @@ class ControllerCheck extends Controller
         if (isset($result_session_check['errors'])) {
             $this->response->setData($result_session_check);
         } else {
-            $user_info = (new GetUser($this->dic))->run($result_session_check['user_id']);
-            if (isset($user_info['data'])) {
-                $user = $user_info['data'];
-            } else {
-                $user = [];
-            }
-
             $this->response->setData([
                 'data' => [
                     'id'   => $result_session_check['session'],
-                    'user' => $user,
                 ],
             ]);
         }
