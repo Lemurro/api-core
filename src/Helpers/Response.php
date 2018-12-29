de93e605622c350245e2a3cf03e8330e345eac6d
Дмитрий Щербаков <atomcms@ya.ru>
2018-12-29 12:57:26 +0500

    feat Добавлены быстрые фасады для возврата часто используемых ошибок

--- src/Helpers/Response.php
+++ src/Helpers/Response.php
@@ -2,8 +2,9 @@
 /**
  * Генератор ответа
  *
- * @version 29.12.2018
+ * @version 24.12.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
+ * @author  Евгений Кулагин <ekulagin59@gmail.com>
  */
 
 namespace Lemurro\Api\Core\Helpers;
@@ -22,8 +23,9 @@ class Response
      *
      * @return array
      *
-     * @version 29.12.2018
+     * @version 24.12.2018
      * @author  Дмитрий Щербаков <atomcms@ya.ru>
+     * @author  Евгений Кулагин <ekulagin59@gmail.com>
      */
     static function data($data)
     {
@@ -39,6 +41,89 @@ class Response
     }
 
     /**
+     * Сгенерируем одну ошибку "400 Bad Request"
+     *
+     * @param string $title Краткое, понятное для человека описание проблемы
+     * @param array  $meta  Массив дополнительных данных об ошибке для приложения
+     *
+     * @return array
+     *
+     * @version 29.12.2018
+     * @author  Дмитрий Щербаков <atomcms@ya.ru>
+     */
+    static function error400($title, $meta = [])
+    {
+        return self::error('400 Bad Request', 'warning', $title, $meta);
+    }
+
+    /**
+     * Сгенерируем одну ошибку "401 Unauthorized"
+     *
+     * @param string $title Краткое, понятное для человека описание проблемы
+     * @param array  $meta  Массив дополнительных данных об ошибке для приложения
+     *
+     * @return array
+     *
+     * @version 29.12.2018
+     * @author  Дмитрий Щербаков <atomcms@ya.ru>
+     */
+    static function error401($title, $meta = [])
+    {
+        return self::error('401 Unauthorized', 'warning', $title, $meta);
+    }
+
+    /**
+     * Сгенерируем одну ошибку "403 Forbidden"
+     *
+     * @param string  $title    Краткое, понятное для человека описание проблемы
+     * @param boolean $redirect Делать редирект на страницу 403 или нет
+     * @param array   $meta     Массив дополнительных данных об ошибке для приложения
+     *
+     * @return array
+     *
+     * @version 29.12.2018
+     * @author  Дмитрий Щербаков <atomcms@ya.ru>
+     */
+    static function error403($title, $redirect, $meta = [])
+    {
+        $meta['redirect'] = $redirect;
+
+        return self::error('403 Forbidden', 'warning', $title, $meta);
+    }
+
+    /**
+     * Сгенерируем одну ошибку "404 Not Found"
+     *
+     * @param string $title Краткое, понятное для человека описание проблемы
+     * @param array  $meta  Массив дополнительных данных об ошибке для приложения
+     *
+     * @return array
+     *
+     * @version 29.12.2018
+     * @author  Дмитрий Щербаков <atomcms@ya.ru>
+     */
+    static function error404($title, $meta = [])
+    {
+        return self::error('404 Not Found', 'info', $title, $meta);
+    }
+
+    /**
+     * Сгенерируем одну ошибку "500 Internal Server Error"
+     *
+     * @param string $title Краткое, понятное для человека описание проблемы
+     * @param array  $meta  Массив дополнительных данных об ошибке для приложения
+     *
+     * @return array
+     *
+     * @version 29.12.2018
+     * @author  Дмитрий Щербаков <atomcms@ya.ru>
+     */
+    static function error500($title, $meta = [])
+    {
+        return self::error('500 Internal Server Error', 'danger', $title, $meta);
+    }
+
+    /**
      * Сгенерируем одну ошибку
      *
      * @param string $status Код состояния HTTP или массив ошибок
@@ -50,6 +135,7 @@ class Response
      *
      * @version 29.12.2018
      * @author  Дмитрий Щербаков <atomcms@ya.ru>
+     * @author  Евгений Кулагин <ekulagin59@gmail.com>
      */
     static function error($status, $code, $title, $meta = [])
     {
@@ -77,7 +163,7 @@ class Response
      *
      * @return array
      *
-     * @version 29.12.2018
+     * @version 24.12.2018
      * @author  Дмитрий Щербаков <atomcms@ya.ru>
      */
     static function errors($errors)
