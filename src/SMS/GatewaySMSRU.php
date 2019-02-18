b6ef8fb4067576c0e9150ce81711015bfcd9d366
Дмитрий Щербаков <atomcms@ya.ru>
2019-02-18 13:58:52 +0500

    feat Добавлена валидация телефона при отправке СМС

--- src/SMS/GatewaySMSRU.php
+++ src/SMS/GatewaySMSRU.php
@@ -2,13 +2,16 @@
 /**
  * Шлюз для отправки sms: sms.ru
  *
- * @version 12.12.2018
+ * @version 18.02.2019
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 
 namespace Lemurro\Api\Core\SMS;
 
 use Lemurro\Api\App\Configs\SettingsSMS;
+use Lemurro\Api\Core\Helpers\LoggerFactory;
+use libphonenumber\NumberParseException;
+use libphonenumber\PhoneNumberUtil;
 
 /**
  * Class GatewaySMSRU
@@ -25,12 +28,20 @@ class GatewaySMSRU
      *
      * @return array
      *
-     * @version 12.12.2018
+     * @version 18.02.2019
      * @author  Дмитрий Щербаков <atomcms@ya.ru>
      */
     public function send($phone, $message)
     {
-        $phone_number = preg_replace('/[^0-9]/', '', $phone);
+        $phone_number = $this->validatePhone($phone);
+
+        if (empty($phone_number)) {
+            return [
+                'success' => false,
+                'message' => 'Неверный номер телефона',
+            ];
+        }
+
         $from = (SettingsSMS::SMSRU_SENDER == '' ? '' : '&from=' . SettingsSMS::SMSRU_SENDER);
         $result = file_get_contents('https://sms.ru/sms/send?api_id=' . SettingsSMS::SMSRU_API_ID . '&to=' . $phone_number . $from . '&text=' . urlencode($message) . '&json=1');
 
@@ -61,5 +72,33 @@ class GatewaySMSRU
                 'message' => 'Ошибка запроса к API',
             ];
         }
+    }
+
+    /**
+     * Валидация телефона
+     *
+     * @param string $phone Номер телефона получателя
+     *
+     * @return string|null
+     *
+     * @version 18.02.2019
+     * @author  Дмитрий Щербаков <atomcms@ya.ru>
+     */
+    protected function validatePhone($phone)
+    {
+        $phoneUtil = PhoneNumberUtil::getInstance();
+
+        try {
+            $phoneNumber = $phoneUtil->parse($phone, 'RU');
+
+            if ($phoneUtil->isPossibleNumber($phoneNumber) && $phoneUtil->isValidNumber($phoneNumber) && $phoneUtil->getNumberType($phoneNumber) === 'MOBILE') {
+                return $phoneNumber->getCountryCode() . $phoneNumber->getNationalNumber();
+            }
+        } catch (NumberParseException $e) {
+            $log = LoggerFactory::create('SMS');
+            $log->error('GatewaySMSRU->validatePhone(): ' . $e->getMessage());
+        }
+
+        return null;
     }
 }
