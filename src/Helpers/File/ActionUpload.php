1103db523621b64465476612cfd944017a42b67c
Дмитрий Щербаков <atomcms@ya.ru>
2019-05-15 15:12:48 +0500

    feat В SettingsFile добавлен новый параметр CHECK_FILE_BY

--- src/Helpers/File/ActionUpload.php
+++ src/Helpers/File/ActionUpload.php
@@ -45,37 +45,23 @@ class ActionUpload extends Action
         $orig_size = $uploaded_file->getSize();
         $orig_ext = $uploaded_file->getClientOriginalExtension();
 
-        if (in_array($orig_mime, SettingsFile::ALLOWED_TYPES)) {
-            if ($orig_size <= SettingsFile::ALLOWED_SIZE) {
-                $dest_folder = SettingsFile::TEMP_FOLDER;
-                $dest_name = md5_file($orig_tmp);
+        switch (SettingsFile::CHECK_FILE_BY) {
+            case 'type':
+                $type_corrected = in_array($orig_mime, SettingsFile::ALLOWED_TYPES);
+                break;
 
-                if (isset($this->dic['user']['id'])) {
-                    $dest_name .= '-' . $this->dic['user']['id'];
-                }
+            case 'ext':
+                $type_corrected = in_array($orig_ext, SettingsFile::ALLOWED_EXTENSIONS);
+                break;
 
-                $file_id = (new FileName())->generate($dest_folder, $dest_name, $orig_ext);
+            default:
+                $log->info('File: Неверный вид проверки типа файла: ' . SettingsFile::CHECK_FILE_BY);
 
-                $uploaded_file->move($dest_folder, $file_id);
+                return Response::error400('Неверные настройки приложения, пожалуйста обратитесь к администратору');
+                break;
+        }
 
-                if (is_readable($dest_folder . $file_id) && is_file($dest_folder . $file_id)) {
-                    return Response::data([
-                        'id' => $file_id,
-                    ]);
-                } else {
-                    $log->error('File: Файл не был загружен', [
-                        'mime'    => $orig_mime,
-                        'size'    => $orig_size,
-                        'ext'     => $orig_ext,
-                        'file_id' => $file_id,
-                    ]);
-
-                    return Response::error500('Файл не был загружен, попробуйте ещё раз');
-                }
-            } else {
-                return Response::error400('Максимальный размер файла: ' . SettingsFile::MAX_SIZE_FORMATED);
-            }
-        } else {
+        if ($type_corrected === false) {
             $log->info('File: Попытка загрузки неразрешённого файла .' . $orig_ext . ': ' . $orig_mime);
 
             $allowed_extensions = implode(', ', SettingsFile::ALLOWED_EXTENSIONS);
@@ -85,5 +71,35 @@ class ActionUpload extends Action
                 'ext'  => $orig_ext,
             ]);
         }
+
+        if ($orig_size > SettingsFile::ALLOWED_SIZE) {
+            return Response::error400('Максимальный размер файла: ' . SettingsFile::MAX_SIZE_FORMATED);
+        }
+
+        $dest_folder = SettingsFile::TEMP_FOLDER;
+        $dest_name = md5_file($orig_tmp);
+
+        if (isset($this->dic['user']['id'])) {
+            $dest_name .= '-' . $this->dic['user']['id'];
+        }
+
+        $file_id = (new FileName())->generate($dest_folder, $dest_name, $orig_ext);
+
+        $uploaded_file->move($dest_folder, $file_id);
+
+        if (!is_readable($dest_folder . $file_id) || !is_file($dest_folder . $file_id)) {
+            $log->error('File: Файл не был загружен', [
+                'mime'    => $orig_mime,
+                'size'    => $orig_size,
+                'ext'     => $orig_ext,
+                'file_id' => $file_id,
+            ]);
+
+            return Response::error500('Файл не был загружен, попробуйте ещё раз');
+        }
+
+        return Response::data([
+            'id' => $file_id,
+        ]);
     }
 }
