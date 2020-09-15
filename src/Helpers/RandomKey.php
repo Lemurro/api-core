1dd98a24002fc80d8c0ab1522a89f46004bca8c4
Дмитрий Щербаков <atomcms@ya.ru>
2020-09-15 18:56:52 +0500

    fix [RandomKey] код изменён для работы с php 7.4

--- src/Helpers/RandomKey.php
+++ src/Helpers/RandomKey.php
@@ -1,16 +1,16 @@
 <?php
+
 /**
  * Генератор случайной строки определенной длины
  *
- * @version 01.01.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
+ *
+ * @version 15.09.2020
  */
 
 namespace Lemurro\Api\Core\Helpers;
 
 /**
- * Class RandomKey
- *
  * @package Lemurro\Api\Core\Helpers
  */
 class RandomKey
@@ -22,16 +22,18 @@ class RandomKey
      *
      * @return string
      *
-     * @version 01.01.2018
      * @author  Дмитрий Щербаков <atomcms@ya.ru>
+     *
+     * @version 15.09.2020
      */
     static function generate($length = 10)
     {
-        $chars = "A1B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6Q7R8S9T0U1V2W3X4Y5Z6a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6";
-        $key = "";
+        $chars = 'A1B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6Q7R8S9T0U1V2W3X4Y5Z6a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6';
+        $end = strlen($chars) - 1;
+        $key = '';
 
         for ($i = 0; $i < $length; $i++) {
-            $key .= $chars{mt_rand(0, strlen($chars) - 1)};
+            $key .= $chars[mt_rand(0, $end)];
         }
 
         return $key;
