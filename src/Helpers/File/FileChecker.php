6324b00048ee4d6397b6bad7ab8e09ba35b388a2
DimNS <atomcms@ya.ru>
2021-11-11 12:39:17 +0500

    fix Удален старый код

--- src/Helpers/File/FileChecker.php
+++ src/Helpers/File/FileChecker.php
@@ -1,47 +1,26 @@
 <?php
-/**
- * Интерфейс проверки доступа к файлу
- *
- * @version 28.03.2019
- * @author  Дмитрий Щербаков <atomcms@ya.ru>
- */
 
 namespace Lemurro\Api\Core\Helpers\File;
 
 use Lemurro\Api\Core\Checker\Checker;
+use Lemurro\Api\Core\Helpers\LoggerFactory;
 use Monolog\Logger;
 use Pimple\Container;
 
 /**
- * Class FileChecker
- *
- * @package Lemurro\Api\Core\Helpers\File
+ * Интерфейс проверки доступа к файлу
  */
 abstract class FileChecker
 {
-    /**
-     * Контейнер
-     *
-     * @var Container
-     */
-    protected $dic;
-
+    protected Container $dic;
     protected Checker $checker;
     protected Logger $log;
 
-    /**
-     * Конструктор
-     *
-     * @param Container $dic Контейнер
-     *
-     * @version 08.01.2019
-     * @author  Дмитрий Щербаков <atomcms@ya.ru>
-     */
-    public function __construct($dic)
+    public function __construct(Container $dic)
     {
         $this->dic = $dic;
         $this->checker = new Checker($dic);
-        $this->log = $dic['logfactory']->create('FileChecker');
+        $this->log = LoggerFactory::create('FileChecker');
     }
 
     /**
@@ -50,9 +29,6 @@ abstract class FileChecker
      * @param string $container_id ИД контейнера
      *
      * @return boolean
-     *
-     * @version 08.01.2019
-     * @author  Дмитрий Щербаков <atomcms@ya.ru>
      */
     abstract public function check($container_id);
 }
