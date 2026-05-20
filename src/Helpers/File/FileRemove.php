1833e79f91f0006f61fac698606bfa78dc1261df
egnotka <info@egnotka.ru>
2026-05-20 11:13:48 +0500

    FileRemove: изменена логика запуска транзакции

--- src/Helpers/File/FileRemove.php
+++ src/Helpers/File/FileRemove.php
@@ -2,11 +2,13 @@
 
 namespace Lemurro\Api\Core\Helpers\File;
 
+use Doctrine\DBAL\Exception;
 use Lemurro\Api\App\Configs\SettingsFile;
 use Lemurro\Api\Core\Abstracts\Action;
 use Lemurro\Api\Core\Helpers\DataChangeLog;
 use Lemurro\Api\Core\Helpers\Response;
 use Pimple\Container;
+use Throwable;
 
 /**
  * Удаление файла
@@ -16,12 +18,12 @@ class FileRemove extends Action
     /**
      * @var FileInfo
      */
-    protected $file_info;
+    protected FileInfo $file_info;
 
     /**
      * @var FileRights
      */
-    protected $file_rights;
+    protected FileRights $file_rights;
 
     /**
      * @param Container $dic Контейнер
@@ -37,44 +39,95 @@ class FileRemove extends Action
     /**
      * Удаление файла
      *
-     * @param integer $fileid ИД файла
+     * @param integer|string $file_id ИД файла
      *
      * @return array
+     * @throws Throwable
      */
-    public function run($fileid)
+    public function run(int|string $file_id): array
     {
-        $info = $this->file_info->getById($fileid);
+        $info = $this->file_info->getById($file_id);
+
         if (empty($info)) {
             return Response::error404('Файл не найден');
         }
 
         if ($this->file_rights->check($info['container_type'], $info['container_id']) === false) {
             return Response::error403('Доступ ограничен', false, [
-                'file_id' => $fileid,
+                'file_id' => $file_id,
             ]);
         }
 
-        $this->dbal->transactional(function () use ($fileid, $info): void {
-            $file_path = SettingsFile::FILE_FOLDER . $info['path'];
-
-            if (SettingsFile::FULL_REMOVE) {
-                $this->dbal->delete('files', ['id' => $fileid]);
-                @unlink($file_path);
-            } else {
-                $this->dbal->update('files', [
-                    'deleted_at' => $this->dic['datetimenow'],
-                ], [
-                    'id' => $fileid
-                ]);
-            }
-
-            /** @var DataChangeLog $datachangelog */
-            $datachangelog = $this->dic['datachangelog'];
-            $datachangelog->insert('files', 'delete', $fileid, $info);
-        });
+        if ($this->isTransactionActive()) {
+            $this->removeFile($file_id, $info);
+        } else {
+            $this->dbal->transactional(function () use ($file_id, $info): void {
+                $this->removeFile($file_id, $info);
+            });
+        }
 
         return Response::data([
-            'id' => $fileid,
+            'id' => $file_id,
         ]);
+    }
+
+    /**
+     * Выполняет удаление файла.
+     *
+     * Метод не управляет транзакцией сам.
+     * Транзакция открывается выше только если она ещё не открыта.
+     *
+     * @param integer|string $file_id ИД файла
+     * @param array $info Данные файла до удаления
+     *
+     * @return void
+     * @throws Exception
+     */
+    protected function removeFile(int|string $file_id, array $info): void
+    {
+        $file_path = SettingsFile::FILE_FOLDER . $info['path'];
+
+        if (SettingsFile::FULL_REMOVE) {
+            $this->dbal->delete('files', [
+                'id' => $file_id,
+            ]);
+
+            @unlink($file_path);
+        } else {
+            $this->dbal->update('files', [
+                'deleted_at' => $this->dic['datetimenow'],
+            ], [
+                'id' => $file_id,
+            ]);
+        }
+
+        /** @var DataChangeLog $datachangelog */
+        $datachangelog = $this->dic['datachangelog'];
+        $datachangelog->insert('files', 'delete', $file_id, $info);
+    }
+
+    /**
+     * Проверяет, открыта ли уже транзакция.
+     *
+     * Сначала проверяем нативное PDO-соединение, потому что внешняя транзакция
+     * может быть открыта через ORM::get_db()->beginTransaction(), а не через DBAL.
+     *
+     * @return boolean
+     */
+    protected function isTransactionActive(): bool
+    {
+        if (method_exists($this->dbal, 'getNativeConnection')) {
+            $connection = $this->dbal->getNativeConnection();
+
+            if (is_object($connection) && method_exists($connection, 'inTransaction')) {
+                return $connection->inTransaction();
+            }
+        }
+
+        if (method_exists($this->dbal, 'isTransactionActive')) {
+            return $this->dbal->isTransactionActive();
+        }
+
+        return false;
     }
 }
